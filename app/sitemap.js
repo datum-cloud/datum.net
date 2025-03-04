551bc1777736bf58ce6e7edbc3392409a78ddb58
Edo <aria.edo@gmail.com>
2025-03-04 15:37:46 +0700

    add event, doc and guide to sitemaps

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,36 +1,83 @@
-import { getBlogs, getChangelogs } from "@/libs/contents"
+import { client } from '@/sanity/lib/client'
 
-export default function sitemap() {
-  const blogs = getBlogs();
-  const changelogs = getChangelogs();
+import { getBlogs, getChangelogs, getPages } from "@/libs/contents"
+import { docsSource, guidesSource } from '@/libs/source';
 
-  var sitemaps = [
-      {
-        url: 'http://localhost:3000/',
+async function getEvents () {
+  const events = await client.fetch('*[_type == "event"]')
+  return events
+}
+export default async function sitemap() {
+  const blogs = getBlogs()
+  const changelogs = getChangelogs()
+  const pages = getPages()
+  const docs = docsSource.getPages()
+  const guides = guidesSource.getPages()
+  const events = await getEvents()
+
+  var sitemaps = []
+
+  // Docs
+  sitemaps.push({
+    url: process.env.APP_URL + '/doc',
+    lastModified: new Date(),
+    changeFrequency: 'yearly',
+    priority: 1,
+  });
+  docs.forEach((doc) => {
+    sitemaps.push({
+      url: process.env.APP_URL + doc.url,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    });
+  });
+
+  // Guides
+  sitemaps.push({
+    url: process.env.APP_URL + '/guide',
+    lastModified: new Date(),
+    changeFrequency: 'yearly',
+    priority: 1,
+  });
+  guides.forEach((guide) => {
+    sitemaps.push({
+      url: process.env.APP_URL + guide.url,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    });
+  });
+
+  // Events
+  sitemaps.push({
+    url: process.env.APP_URL + '/events',
+    lastModified: new Date(),
+    changeFrequency: 'yearly',
+    priority: 1,
+  });
+  events.forEach((event) => {
+    sitemaps.push({
+      url: process.env.APP_URL + `/events/` + event.slug.current,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    });
+  });
+
+  // Static pages
+  Object.keys(pages).forEach((page) => {
+    if (page !== 'home') {
+      sitemaps.push({
+        url: 'http://localhost:3000/' + page,
         lastModified: new Date(),
         changeFrequency: 'yearly',
-        priority: 1,
-      },
-      {
-        url: 'http://localhost:3000/about',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: 'http://localhost:3000/blog',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
         priority: 0.5,
-      },
-      {
-        url: 'http://localhost:3000/changelog',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.5,
-      },
-    ]
+      });
+    }
+  })
 
+  // Blogs
   blogs.forEach((blog) => {
     sitemaps.push({
       url: 'http://localhost:3000/blog/' + blog.slug,
@@ -40,6 +87,7 @@ export default function sitemap() {
     });
   });
 
+  // Changelog
   changelogs.forEach((log) => {
     sitemaps.push({
       url: 'http://localhost:3000/changelog/' + log.slug,

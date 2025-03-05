ee8de13259bd3d70dd825067a7d1651a0c7e5099
Ronggur Habibun <ronggur.mh@gmail.com>
2025-03-05 14:50:14 +0700

    Changelog
    - changelog page
    - seo
    - sitema

---
+++ src/content/config.ts
@@ -0,0 +1,28 @@
+import { z, defineCollection } from 'astro:content';
+
+// Define the schema for changelog entries
+const changelogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    version: z.string().optional(),
+    summary: z.string(),
+  }),
+});
+
+// Define pages collections
+const pagesCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    isHomePage: z.boolean().optional().default(false),
+    slug: z.string().optional(),
+    order: z.number().optional().default(999)
+  }),
+});
+
+// Export all collections
+export const collections = {
+  'pages': pagesCollection,
+  'changelog': changelogCollection,
+};

dc4d8d212f6d2c2a8994a1813f2dc31b8aa079fc
Ronggur Habibun <ronggur.mh@gmail.com>
2025-08-20 13:45:38 +0700

    feat: v1 changelog

---
+++ src/types/changelog.ts
@@ -0,0 +1,23 @@
+// src/types/changelog.ts
+
+export interface ChangelogItem {
+  id: string;
+  date: Date;
+  title: string;
+  description: string;
+  tags: ChangelogTag[];
+}
+
+export interface ChangelogTag {
+  label: string;
+  type: 'fixed' | 'new' | 'changed';
+  value?: string;
+}
+
+export interface ChangelogCardProps {
+  id: string;
+  date: Date;
+  title: string;
+  description: string;
+  tags: ChangelogTag[];
+}

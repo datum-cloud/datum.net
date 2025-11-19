646327f28be3dcc76e7018326d0de8f5dad700ad
AriaEdo <aria.edo@gmail.com>
2025-11-19 07:34:25 +0700

    fix: breadcrumb and update dev path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,35 @@
 import { stargazerCount } from '@libs/datum';
-import { defineMiddleware } from 'astro:middleware';
+import { sequence } from 'astro:middleware';
+import type { MiddlewareHandler } from 'astro';
 
-export const onRequest = defineMiddleware(async (context, next) => {
-  const tempStarCount = await stargazerCount();
+const PROTECTED_ROUTES = [/^\/dev($|\/.*)/];
 
+const isProtected = (path: string): boolean => {
+  return PROTECTED_ROUTES.some((pattern) => pattern.test(path));
+};
+
+const routeGuard: MiddlewareHandler = async ({ url, redirect }, next) => {
+  const mode = process.env.MODE || import.meta.env.MODE;
+  const pathName = new URL(url).pathname;
+
+  if (isProtected(pathName)) {
+    // only for development mode, to ease testing
+    if (mode == 'production') {
+      return redirect(`/`);
+    }
+  }
+
+  return next();
+};
+
+const baseMiddleware: MiddlewareHandler = async (context, next) => {
+  const starCount = await stargazerCount();
   const formatter = new Intl.NumberFormat('en-US', { notation: 'compact' });
-  const formattedStarCount = formatter.format(tempStarCount);
+  const formattedStarCount = formatter.format(starCount);
 
   context.locals.starCount = () => formattedStarCount;
 
   return next();
-});
+};
+
+export const onRequest = sequence(routeGuard, baseMiddleware);
